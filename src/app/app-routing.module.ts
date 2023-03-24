a202ce033b06815ba5d343e89f2e1ea37393358d
Mathias Myrold <Mattimy99@gmail.com>
2023-03-24 18:48:34 +0100

    added navbar logo homepage, notfound,login and emial small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/master/home/home.component';
 import { LoginComponent } from './security/login/login.component';
 import { RegisterComponent } from './security/register/register.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
@@ -7,7 +8,7 @@ import { NotFoundComponent } from './shared/not-found/not-found.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
