8567a6471acfa33fa880527d488d86e29e9dac01
Mathias Myrold <Mattimy99@gmail.com>
2023-03-28 18:49:52 +0200

    started on displaying trips

--- src/app/components/pages/trip/trip.component.ts
+++ src/app/components/pages/trip/trip.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-trip',
   templateUrl: './trip.component.html',
-  styleUrls: ['./trip.component.css']
+  styleUrls: ['./trip.component.css'],
 })
-export class TripComponent {
+export class TripComponent /* implements OnInit */ {
+  /* trip: Trip;
+  id: string;
 
+  constructor(private tripService TripService, private router: Router, private route: ActivatedRoute){
+    this.id ="0";
+    this.trip = new Trip();
+  }
+
+  ngOnInit(): void {
+      this.id = this.route.snapshot.params["id"];
+
+      this.tripService.getTrip(this.id).subscribe ((trip) => {
+        this.trip = trip;
+        console.log("Display trip: " + trip);
+      })
+  }
+  goBack(){
+    this.router.navigate(["/"]);
+  }
+ */
 }
--- src/app/components/pages/trips/trips.component.ts
+++ src/app/components/pages/trips/trips.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-trips',
   templateUrl: './trips.component.html',
-  styleUrls: ['./trips.component.css']
+  styleUrls: ['./trips.component.css'],
 })
-export class TripsComponent {
+export class TripsComponent /* implements OnInit */ {
+  /* trip: Trip[];
+  id: string;
 
+  constructor(private tripService TripService, private router: Router, private route: ActivatedRoute){
+    this.trip = [];
+  }
+
+  ngOnInit(): void {
+      this.id = this.route.snapshot.params["id"];
+
+      this.tripService.getTrip(this.id).subscribe ((trip) => {
+        this.trip = trip;
+        console.log("Display trip: " + trip);
+      })
+  }
+  goBack(){
+    this.router.navigate(["/"]);
+  }
+  displayTrip(id: string){
+    this.router.navigate(["/trips/" + id]);
+  }
+ */
 }
