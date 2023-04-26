acbbaabf89073e9c29a16bb4f12dabc4c476c5c3
haavarty <haavarty@stud.ntnu.no>
2023-04-26 19:00:09 +0200

    fix refresh error

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -28,7 +28,6 @@ export class LoginComponent implements OnInit {
     const password = this.loginForm.value.password;
     this.authService.login(email, password).subscribe((res: any) => {
       if (res['id'] != null && res['token'] != null) {
-        localStorage.setItem('id', res['id']);
         localStorage.setItem('token', res['token']);
         this.router.navigate(['/']);
       }
